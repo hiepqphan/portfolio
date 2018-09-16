500722fd64a5a40c1d85a5f60ce29b1385474c8c
Hiep Phan <phan.1998@hotmail.com>
2018-09-15 23:32:40 -0400

    changed theme

--- public/mainjs.js
+++ public/mainjs.js
@@ -1,3 +1,18 @@
+var header = document.getElementById("cover-img");
+var sidemenu = document.getElementById("side-menu-container");
+var header_toppos = header.offsetTop;
+
+$(window).scroll(function() {
+	if (window.pageYOffset >= header_toppos) {
+		sidemenu.classList.add("sticky");
+		$("#side-menu li a").css('margin-left','-45px');
+	}
+	else {
+		sidemenu.classList.remove("sticky");
+		$("#side-menu li a").css('margin-left','-100px');
+	}
+});
+
 $('#button-down-button, .navbar-left .menu-left').click(function() {
 	$('html, body').animate({
 		scrollTop: $($(this).attr('href')).offset().top
