0bff556184186a1baa6341a6813319c4829debae
Hiep Phan <hiepqphan@hotmail.com>
2019-07-15 15:34:19 -0400

    Updates

---
+++ precache-manifest.c8302c4be95f816d4cecde4e890e9539.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a33e15b0d6d590f102ec51e4f6d505",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "84f2f89f16c68f5e9d9c",
+    "url": "/portfolio/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "b6a12ae3cb8bfcc8a933",
+    "url": "/portfolio/static/css/main.943baf1d.chunk.css"
+  },
+  {
+    "revision": "84f2f89f16c68f5e9d9c",
+    "url": "/portfolio/static/js/2.7cc9696a.chunk.js"
+  },
+  {
+    "revision": "b6a12ae3cb8bfcc8a933",
+    "url": "/portfolio/static/js/main.23e9cb0c.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "d236fa9ca3af879094c1aa8ed5c37cf5",
+    "url": "/portfolio/static/media/background.d236fa9c.png"
+  },
+  {
+    "revision": "170869ee49211c43aeb7741bd01d2cc3",
+    "url": "/portfolio/static/media/book-user.170869ee.svg"
+  },
+  {
+    "revision": "1e879f3f922b6ab77c1aa1f4d9202ee4",
+    "url": "/portfolio/static/media/pen.1e879f3f.svg"
+  },
+  {
+    "revision": "69c22db200b5be321423fd5a85c064bc",
+    "url": "/portfolio/static/media/three-lines.69c22db2.svg"
+  },
+  {
+    "revision": "48984182bd1b333e550ebe8a8f763397",
+    "url": "/portfolio/static/media/user.48984182.svg"
+  },
+  {
+    "revision": "e079903d4f03822c1465d5775e235136",
+    "url": "/portfolio/static/media/x.e079903d.svg"
+  }
+]);
