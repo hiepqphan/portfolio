3ddec4749d827f09ec7dd57e9be72e3375d659e4
Hiep Phan <hiepqphan@hotmail.com>
2019-07-15 16:30:26 -0400

    Fixes for Github pages

--- src/App.js
+++ src/App.js
@@ -11,35 +11,35 @@ import Contact from "./components/Contact/contact";
 import Wolley from "./components/Wolley/wolley";
 import './App.css';
 
-library.add(fab, faEnvelope);  
+library.add(fab, faEnvelope);
 
 class App extends Component {
   componentDidMount() {
   }
-  
+
   render() {
     let aboutPage = () => (
       <FirebaseContext.Consumer>
         {firebase => <About firebase={firebase}/>}
       </FirebaseContext.Consumer>);
-      
+
     let projectPage = () => (
       <FirebaseContext.Consumer>
         {firebase => <Project firebase={firebase}/>}
       </FirebaseContext.Consumer>);
-      
+
     let contactPage = () => (
       <FirebaseContext.Consumer>
         {firebase => <Contact firebase={firebase}/>}
       </FirebaseContext.Consumer>);
-      
+
     return (
       <Router>
-        <Route exact path="/" component={() => <Redirect to="/about"/>}/>
-        <Route path="/about" component={aboutPage}/>
-        <Route path="/project" component={projectPage}/>
-        <Route path="/contact" component={contactPage}/>
-        <Route path="/wolley" component={Wolley}/>
+        <Route exact path="/portfolio" component={() => <Redirect to="/portfolio/about"/>}/>
+        <Route path="/portfolio/about" component={aboutPage}/>
+        <Route path="/portfolio/project" component={projectPage}/>
+        <Route path="/portfolio/contact" component={contactPage}/>
+        <Route path="/portfolio/wolley" component={Wolley}/>
       </Router>
     );
   }
